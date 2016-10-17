2d410e3080ccfb92654f1442c07cdfbe291362ed
tranb3r <tranb3r@gmail.com>
2016-10-17 11:53:24 +0200

    Android: use OkHttp3

---
+++ SecureHttpClient.Android/NativeCookieJar.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using Java.Net;
+using Square.OkHttp3;
+
+namespace SecureHttpClient
+{
+    internal class NativeCookieJar : Java.Lang.Object, ICookieJar
+    {
+        private readonly CookieManager _cookieManager;
+
+        public NativeCookieJar()
+        {
+            _cookieManager = new CookieManager();
+        }
+
+        public IList<Cookie> LoadForRequest(HttpUrl httpUrl)
+        {
+            // No domain matching done here, because OkHttp.Cookie's "hostonly" does not exist in Java.Net.HttpCookie
+            return _cookieManager.CookieStore.Cookies.Select(ToNetCookie).ToList();
+        }
+
+        public void SaveFromResponse(HttpUrl url, IList<Cookie> cookies)
+        {
+            foreach (var cookie in cookies.Select(ToNativeCookie))
+            {
+                _cookieManager.CookieStore.Add(new URI(cookie.Domain), cookie);
+            }
+        }
+
+        private static HttpCookie ToNativeCookie(Cookie cookie)
+        {
+            return new HttpCookie(cookie.Name(), cookie.Value())
+            {
+                Domain = cookie.Domain(),
+                Path = cookie.Path(),
+                Secure = cookie.Secure(),
+            };
+        }
+
+        private static Cookie ToNetCookie(HttpCookie cookie)
+        {
+            var cookieBuilder = new Cookie.Builder().Name(cookie.Name).Value(cookie.Value).Path(cookie.Path).Domain(cookie.Domain);
+            if (cookie.Secure)
+            {
+                cookieBuilder.Secure();
+            }
+            return cookieBuilder.Build();
+        }
+    }
+}
