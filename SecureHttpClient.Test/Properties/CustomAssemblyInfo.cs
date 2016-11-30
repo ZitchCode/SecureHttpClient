6497a79a0ec6a08b0b62c3be6c337789c22dac84
tranb3r <tranb3r@gmail.com>
2016-11-30 18:25:32 +0100

    Version 1.4.2

--- SecureHttpClient.Test/Properties/CustomAssemblyInfo.cs
+++ SecureHttpClient.Test/Properties/CustomAssemblyInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.4.1")]
-[assembly: System.Reflection.AssemblyFileVersion("1.4.1")]
+[assembly: System.Reflection.AssemblyVersion("1.4.2")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.2")]
 
 
