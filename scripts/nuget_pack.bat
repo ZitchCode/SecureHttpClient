59757bc861a74e75a0bda3e4c4c2b931236f363a
tranb3r <tranb3r@gmail.com>
2017-07-01 22:51:45 +0200

    Fix nuget

--- scripts/nuget_pack.bat
+++ scripts/nuget_pack.bat
@@ -3,11 +3,20 @@
 SET MacServerAddress=192.168.118.128
 SET MacServerUser=bertrand
 
+echo -- GET VERSION ---------------------------------------------------------------------------------------------------------------------------------------------
+set /p Version=<..\version.txt
+echo Version: %Version%
+
 echo -- CLEAN ---------------------------------------------------------------------------------------------------------------------------------------------------
 msbuild /v:m ../SecureHttpClient.Android/SecureHttpClient.Android.csproj /p:Configuration=Release /t:Clean
 msbuild /v:m ../SecureHttpClient.iOS/SecureHttpClient.iOS.csproj /p:Configuration=Release /t:Clean /p:ServerAddress=%MacServerAddress% /p:ServerUser=%MacServerUser%
 msbuild /v:m ../SecureHttpClient.Portable/SecureHttpClient.Portable.csproj /p:Configuration=Release /t:Clean
 if exist "..\bin\" rd /s/q "..\bin"
+
+echo -- RESTORE -------------------------------------------------------------------------------------------------------------------------------------------------
+msbuild /v:m ../SecureHttpClient.Android/SecureHttpClient.Android.csproj /p:Configuration=Release /t:Restore
+msbuild /v:m ../SecureHttpClient.iOS/SecureHttpClient.iOS.csproj /p:Configuration=Release /t:Restore /p:ServerAddress=%MacServerAddress% /p:ServerUser=%MacServerUser%
+msbuild /v:m ../SecureHttpClient.Portable/SecureHttpClient.Portable.csproj /p:Configuration=Release /t:Restore
 
 echo -- BUILD ---------------------------------------------------------------------------------------------------------------------------------------------------
 msbuild /v:m ../SecureHttpClient.Android/SecureHttpClient.Android.csproj /p:Configuration=Release
@@ -16,13 +25,9 @@ msbuild /v:m ../SecureHttpClient.Portable/SecureHttpClient.Portable.csproj /p:Co
 
 echo -- MERGE ---------------------------------------------------------------------------------------------------------------------------------------------------
 mkdir "..\bin"
-..\..\ILRepack.exe /out:..\bin\MonoAndroid\SecureHttpClient.dll ..\SecureHttpClient.Android\bin\Release\SecureHttpClient.dll ..\SecureHttpClient.Android\bin\Release\SecureHttpClient.Abstractions.dll
-..\..\ILRepack.exe /out:..\bin\Xamarin.iOS\SecureHttpClient.dll ..\SecureHttpClient.iOS\bin\Release\SecureHttpClient.dll ..\SecureHttpClient.CertificatePinning\bin\Release\SecureHttpClient.CertificatePinning.dll ..\SecureHttpClient.iOS\bin\Release\SecureHttpClient.Abstractions.dll
-..\..\ILRepack.exe /out:..\bin\netstandard1.3\SecureHttpClient.dll ..\SecureHttpClient.Portable\bin\Release\SecureHttpClient.dll ..\SecureHttpClient.CertificatePinning\bin\Release\SecureHttpClient.CertificatePinning.dll ..\SecureHttpClient.Portable\bin\Release\SecureHttpClient.Abstractions.dll
-
-echo -- GET VERSION ---------------------------------------------------------------------------------------------------------------------------------------------
-set /p Version=<..\version.txt
-echo Version: %Version%
+..\..\ILRepack.exe /out:..\bin\MonoAndroid\SecureHttpClient.dll /ver:%Version% ..\SecureHttpClient.Android\bin\Release\SecureHttpClient.dll ..\SecureHttpClient.Android\bin\Release\SecureHttpClient.Abstractions.dll
+..\..\ILRepack.exe /out:..\bin\Xamarin.iOS\SecureHttpClient.dll /ver:%Version% ..\SecureHttpClient.iOS\bin\Release\SecureHttpClient.dll ..\SecureHttpClient.iOS\bin\Release\SecureHttpClient.CertificatePinning.dll ..\SecureHttpClient.iOS\bin\Release\SecureHttpClient.Abstractions.dll
+..\..\ILRepack.exe /out:..\bin\netstandard1.3\SecureHttpClient.dll /ver:%Version% ..\SecureHttpClient.Portable\bin\Release\netstandard1.3\SecureHttpClient.dll ..\SecureHttpClient.Portable\bin\Release\netstandard1.3\SecureHttpClient.CertificatePinning.dll ..\SecureHttpClient.Portable\bin\Release\netstandard1.3\SecureHttpClient.Abstractions.dll
 
 echo -- NUGET PACK ----------------------------------------------------------------------------------------------------------------------------------------------
 ..\..\nuget.exe pack ..\SecureHttpClient.nuspec -OutputDir ..\ -version %Version%
