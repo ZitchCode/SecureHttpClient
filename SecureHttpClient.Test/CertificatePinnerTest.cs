32e544b90f729c22ba76b483f2830462fc0c0544
tranb3r <tranb3r@gmail.com>
2019-12-09 10:58:20 +0100

    fix pin

--- SecureHttpClient.Test/CertificatePinnerTest.cs
+++ SecureHttpClient.Test/CertificatePinnerTest.cs
@@ -7,7 +7,7 @@ namespace SecureHttpClient.Test
     {
         private const string Hostname = @"www.howsmyssl.com";
         private const string Page = @"https://www.howsmyssl.com/a/check";
-        private static readonly string[] PinsOk = { @"sha256/XFrw+nN3Xz2SUWLbH3+T4xfLQpwGoqY0emXaSPapqtI=" };
+        private static readonly string[] PinsOk = { @"sha256/t9m1w+JfolQ06TCH1SMZsQhUdYgu1lFIpajwn2iSOBo=" };
         private static readonly string[] PinsKo = { @"sha256/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=" };
 
         private const string Hostname2 = @"github.com";
