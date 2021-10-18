26072e1519237feaf46e830aeef634a42c53bec0
tranb3r <tranb3r@gmail.com>
2021-10-18 14:18:14 +0200

    minor change for ios

--- SecureHttpClient.TestRunner.iOS/Main.cs
+++ SecureHttpClient.TestRunner.iOS/Main.cs
@@ -6,7 +6,7 @@ namespace SecureHttpClient.TestRunner.iOS
     {
         private static void Main(string[] args)
         {
-            UIApplication.Main(args, null, nameof(AppDelegate));
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
