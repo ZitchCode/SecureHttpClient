c1a156ac0e887121c931f4630c6589cf701938e6
tranb3r <tranb3r@gmail.com>
2022-07-26 11:28:41 +0200

    add maui test runner

---
+++ SecureHttpClient.TestRunner.Maui/MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Xunit.Runners.Maui;
+
+namespace SecureHttpClient.TestRunner.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            return MauiApp
+                .CreateBuilder()
+                .ConfigureTests(new TestOptions
+                {
+                    Assemblies =
+                    {
+                        typeof (Test.SslTest).GetTypeInfo().Assembly
+                    }
+                })
+                .UseVisualRunner()
+                .Build();
+        }
+    }
+}
---
+++ SecureHttpClient.TestRunner.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SecureHttpClient.TestRunner.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
