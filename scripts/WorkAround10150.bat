ed52bbe3cdca07eb8f80e0608695a0bde0c3ad49
tranb3r <tranb3r@gmail.com>
2022-09-28 14:46:52 +0200

    workaround for maui issue #10150

---
+++ scripts/WorkAround10150.bat
@@ -0,0 +1,3 @@
+xcopy ..\SecureHttpClient\bin\Debug\net6.0-android\*.* ..\SecureHttpClient\bin\Debug\net6.0-android\android-arm64\*.* /Y
+xcopy ..\SecureHttpClient.OkHttp\bin\Debug\net6.0-android\*.* ..\SecureHttpClient.OkHttp\bin\Debug\net6.0-android\android-arm64\*.* /Y
+xcopy ..\SecureHttpClient.Test\bin\Debug\net6.0-android\*.* ..\SecureHttpClient.Test\bin\Debug\net6.0-android\android-arm64\*.* /Y
