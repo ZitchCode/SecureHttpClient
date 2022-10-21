4f5e4864a90053dcb5294e4849826fd9231bb2d3
tranb3r <tranb3r@gmail.com>
2022-10-21 10:13:16 +0200

    remove workaround for maui issue #10150, seems to work again with new versions of vs

--- scripts/WorkAround10150.bat
+++
@@ -1,3 +0,0 @@
-xcopy ..\SecureHttpClient\bin\Debug\net6.0-android\*.* ..\SecureHttpClient\bin\Debug\net6.0-android\android-arm64\*.* /Y
-xcopy ..\SecureHttpClient.OkHttp\bin\Debug\net6.0-android\*.* ..\SecureHttpClient.OkHttp\bin\Debug\net6.0-android\android-arm64\*.* /Y
-xcopy ..\SecureHttpClient.Test\bin\Debug\net6.0-android\*.* ..\SecureHttpClient.Test\bin\Debug\net6.0-android\android-arm64\*.* /Y
